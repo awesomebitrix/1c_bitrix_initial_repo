1df1f33216a14c294778b5ceb939cc6bd8a10ee1
Stanislav <bfday@users.noreply.github.com>
2016-01-31 22:40:33 +0500

    Update scm.php

--- .rocketeer/scm.php
+++ .rocketeer/scm.php
@@ -10,7 +10,7 @@ return [
 
     // The SSH/HTTPS address to your repository
     // Example: https://github.com/vendor/website.git
-    'repository' => 'ssh://git@git.crtweb.ru:22681/va_crtweb/totaldict.git',
+    'repository' => 'ssh://',
 
     // The repository credentials : you can leave those empty
     // if you're using SSH or if your repository is public
