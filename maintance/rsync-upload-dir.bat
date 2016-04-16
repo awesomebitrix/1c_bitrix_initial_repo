92ae440531ef96edfd1ef925b607eb9f4d36ac5e
Stepanov Stanislav <stepanovstanisl@gmail.com>
2016-04-16 22:36:50 +0500

    fix

--- maintance/rsync-upload-dir.bat
+++ maintance/rsync-upload-dir.bat
@@ -1,9 +1,9 @@
 @echo off
-SET USER=vmebel
-SET HOST=victoria-mebel.crtweb.ru
+SET USER=test
+SET HOST=test
 SET REMOTE_UPLOAD_DIR='./public_html/current/web/upload'
 SET LOCAL_DEST_DIR='../web/'
-SET SSH_PORT=7418
+SET SSH_PORT=22
 SET PATH_TO_PRIVATE_KEY=%HOMEPATH%/.ssh/id_rsa
 SET RSYNC=win_utils\cwRsync_5.5.0_x86_Free\bin\rsync
 SET RSYNC_SSH=win_utils\cwRsync_5.5.0_x86_Free\bin\ssh
